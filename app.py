77bd62bde01988f5179f781c6785b72b0ec82e46
awongh <akira@awongh.com>
2021-08-10 16:23:19 +0800

    working python bot example

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
-import twitter
 
 flask_app = Flask(__name__)
 app = App(
-  token="...",
-  signing_secret="..."
-)
-api = twitter.Api(
-  consumer_key='...',
-  consumer_secret='...',
-  access_token_key='...',
-  access_token_secret='...'
+  token=os.environ.get("SLACK_BOT_TOKEN"),
+  signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 handler = SlackRequestHandler(app)
 
 @app.event("app_mention")
 def event_test(body, say, logger):
   result = body["event"]["text"].split("Post this -")
-  if len(result) == 2:
-    status = api.PostUpdate(result[1].strip())
-    link = "https://twitter.com/"+status.user.screen_name+"/status/"+str(status.id)
-    say("Here's your tweet: " + link)
+  result[1].strip()
+  say("Hello!!! "+result[1].strip())
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
